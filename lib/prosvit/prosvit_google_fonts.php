889c62dc9e13e6bbe6cf1a8c2cfaa04ac129ecca
DESKTOP-5H4O0LU\danic <prosvitco@gmail.com>
2017-04-28 13:33:23 -0700

    Google Fonts

---
+++ lib/prosvit/prosvit_google_fonts.php
@@ -0,0 +1,12 @@
+<?php 
+	// define('GOOGLE_FONTS', 'Montserrat:100,200,300,400,500,500i,600,700,800,900'); add to the functions
+
+
+function prosvit_google_fonts() {
+      
+  if( ! defined( 'GOOGLE_FONTS' ) ) return;
+  echo '<link href="//fonts.googleapis.com/css?family=' . GOOGLE_FONTS . '" rel="stylesheet" type="text/css" />'."\n";
+
+}
+
+add_action( 'wp_head', 'prosvit_google_fonts' , 1);
